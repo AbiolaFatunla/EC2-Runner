7dd6fd9897944e79bfec9dca95f95b91494392f5
AbiolaFatunla <110745451+AbiolaFatunla@users.noreply.github.com>
2024-12-22 12:32:12 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = ****CHANGEBUCKETNAME***
+    bucket         = "abiolabucket"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
